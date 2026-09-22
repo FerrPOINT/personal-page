5d1c4d9efbcc54a5f607d52b4c829528e0fd6738
FerrPOINT <ferruspoint@mail.ru>
2026-09-22 11:10:49 +0700

    tune: increase blaster interception range

--- frontend/src/components/heroScenePhysics.ts
+++ frontend/src/components/heroScenePhysics.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-export const BLASTER_ATTACK_RANGE = 1.65;
+export const BLASTER_ATTACK_RANGE = 2;
 export const BLASTER_BEAM_DURATION = 0.32;
 export const BLASTER_MUZZLE_OFFSET = 0.16;
 export const METEOR_SURFACE_OFFSET = 0.22;
